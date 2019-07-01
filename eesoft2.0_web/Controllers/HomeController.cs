2d9cb7404d0b77dd331861724192d667c7b1bd97
Ramu Nimmala <ramun@goggery.com>
2019-07-01 22:26:43 +0530

    Initial Commit
    Uploading frame work

---
+++ eesoft2.0_web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eesoft2._0_web.Controllers
+{
+    public class HomeController : EESOFTBaseController
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
