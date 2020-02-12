ecf6e7e8d5bc27ab859865eabd047e1d3cb249a8
ramun@goggery.com <ramun@goggery.com>
2020-02-12 16:03:55 +0530

    Initial push

---
+++ eesoft2.0_web/Controllers/eesoftBaseController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eesoft2._0_web.Controllers
+{
+    public class EESOFTBaseController : Controller
+    {
+       
+
+    }
+}
---
+++ eesoft2.0_web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="eesoft2._0_web.MvcApplication" Language="C#" %>
